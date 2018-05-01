53a8adea0e8eab431cb62a7c10c2a1f4b7ebe27c
Freezie75 <nathanbittoun@yahoo.fr>
2018-05-01 18:34:20 +0200

    Ajout des films

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(title: "Warcraft", release_year: 2016, director: "Duncan Jones")
+Movie.create(title: "Intouchable", release_year: 2011, director: "Olivier Nakache, Éric Toledano")
+Movie.create(title: "Harry Potter et la coupe de feu", release_year: 2005, director: "Mike Newell")
+Movie.create(title: "Avatar", release_year: 2009, director: "James Cameron")
+Movie.create(title: "Hunger Games", release_year: 2012, director: "Gary Ross")
+Movie.create(title: "Passengers", release_year: 2016, director: "Morten Tyldum")
+Movie.create(title: "Polisse", release_year: 2011, director: "Maïwenn")
